9ff55cee4458a35edff32154b5841ddff8f5c222
xaverw <67040628+Wxyim@users.noreply.github.com>
2026-07-20 10:08:29 +0800

    chore: rewrite

---
+++ modules/performance/baselineprofile/src/main/kotlin/com/github/nomadboxlab/monadbox/performance/StartupBenchmarks.kt
@@ -0,0 +1,55 @@
+/*
+ * This file is part of MonadBox.
+ *
+ * MonadBox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) MonadBox Contributors 2026 - Present
+ */
+
+package com.github.nomadboxlab.monadbox.performance
+
+import androidx.benchmark.macro.BaselineProfileMode
+import androidx.benchmark.macro.CompilationMode
+import androidx.benchmark.macro.StartupMode
+import androidx.benchmark.macro.StartupTimingMetric
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class StartupBenchmarks {
+    @get:Rule val rule = MacrobenchmarkRule()
+
+    @Test fun startupCompilationNone() = benchmark(CompilationMode.None())
+
+    @Test
+    fun startupCompilationBaselineProfiles() =
+        benchmark(CompilationMode.Partial(baselineProfileMode = BaselineProfileMode.UseIfAvailable))
+
+    private fun benchmark(compilationMode: CompilationMode) {
+        rule.measureRepeated(
+            packageName = BenchmarkConfig.TargetPackage,
+            metrics = listOf(StartupTimingMetric()),
+            compilationMode = compilationMode,
+            startupMode = StartupMode.COLD,
+            iterations = 10,
+            setupBlock = { pressHome() },
+            measureBlock = { startupJourney() },
+        )
+    }
+}
